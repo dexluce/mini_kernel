fdd3577aa9fbcae81fba8198062b2be776adf272
Balthazar de Moncuit de Boiscuillé <balthazar.de.moncuit@gmail.com>
2015-11-03 11:13:53 +0100

    add periferique_lib with entête and include it

---
+++ kernel/periferique_lib.h
@@ -0,0 +1,15 @@
+#ifndef _PERIFERIQUE_LIB_H_
+#define _PERIFERIQUE_LIB_H_
+
+#include "../common/types.h"
+
+//Ecrit à l'adresse port la valeur 8-bits data :
+void outb(uint16_t port, uint8_t data);
+//Ecrit à l'adresse port la valeur 8-bits data :
+void outw(uint16_t port, uint16_t data);
+//Retourne 8-bits lus à l'adresse port :
+uint8_t inb(uint16_t port);
+//Retourne 16-bits lus à l'adresse port :
+uint16_t inw(uint16_t port);
+
+#endif
